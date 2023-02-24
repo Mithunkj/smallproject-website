26cd35ddbf2e2e31137a182fb40198579b055c47
Mithunkj <121790267+Mithunkj@users.noreply.github.com>
2023-02-24 11:20:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,439 @@
+// const btnEl = document.getElementById("btn");
+
+// function addNote(){
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     console.log(noteObj);
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+
+// function createNoteEl(id,content){
+//     console.log(id,content)
+// }
+
+// function addNote(){
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+//    // console.log(noteObj);
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     })
+// }
+
+// function deleteNote(){
+
+// }
+
+// function updateNote(){
+
+// }
+
+// function addNote(){
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+//    // console.log(noteObj);
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+// const appEl = document.getElementById("app");
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     //console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     });
+
+
+//     return element;
+
+// }
+
+// function deleteNote(){}
+
+// function updateNote(){}
+
+// function addNote(){
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+//     appEl.insertBefore(noteEl, btnEl);
+
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+// const appEl = document.getElementById("app");
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     //console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     });
+
+
+//     return element;
+
+// }
+
+// function deleteNote(){}
+
+// function updateNote(){}
+
+// function addNote(){
+//     const notes = [];
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+//     appEl.insertBefore(noteEl, btnEl);
+
+//     notes.push(noteObj);
+
+//     saveNote(notes);
+
+// }
+
+// function saveNote(notes){
+//     localStorage.setItem("note-app", JSON.stringify(notes));
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+// const appEl = document.getElementById("app");
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     //console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     });
+
+
+//     return element;
+
+// }
+
+// function deleteNote(){}
+
+// function updateNote(){}
+
+// function addNote(){
+//     const notes = getNotes();
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+//     appEl.insertBefore(noteEl, btnEl);
+
+//     notes.push(noteObj);
+
+//     saveNote(notes);
+
+// }
+
+// function saveNote(notes){
+//     localStorage.setItem("note-app", JSON.stringify(notes));
+// }
+
+// function getNotes(){
+//    return JSON.parse(localStorage.getItem("note-app") || "[]");
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+// const appEl = document.getElementById("app");
+
+// getNotes().forEach((note) => {
+//     const noteEl = createNoteEl(note.id,note.content);
+//     appEl.insertBefore(noteEl,btnEl);
+// });
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     //console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     });
+
+
+//     return element;
+
+// }
+
+// function deleteNote(){}
+
+// function updateNote(){}
+
+// function addNote(){
+//     const notes = getNotes();
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+//     appEl.insertBefore(noteEl, btnEl);
+
+//     notes.push(noteObj);
+
+//     saveNote(notes);
+
+// }
+
+// function saveNote(notes){
+//     localStorage.setItem("note-app", JSON.stringify(notes));
+// }
+
+// function getNotes(){
+//    return JSON.parse(localStorage.getItem("note-app") || "[]");
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+
+// const btnEl = document.getElementById("btn");
+// const appEl = document.getElementById("app");
+
+// getNotes().forEach((note) => {
+//     const noteEl = createNoteEl(note.id,note.content);
+//     appEl.insertBefore(noteEl,btnEl);
+// });
+
+// function createNoteEl(id,content){
+//     //console.log(id,content)
+//     const element = document.createElement("textarea");
+//     element.classList.add("note");
+//     element.placeholder = "Empty Note";
+//     element.value = content;
+    
+//     //console.log(element);
+
+//     element.addEventListener("dblclick", () => {
+//         const warning = confirm("Do you want to delete this note?");
+//         if(warning){
+//             deleteNote(id,element);
+//         }
+//     });
+
+//     element.addEventListener("input", () => {
+//         updateNote(id, element.value);
+//     });
+
+
+//     return element;
+
+// }
+
+// function deleteNote(){}
+
+// function updateNote(id, content){
+//     const notes = getNotes();
+//     const target = notes.filter((note) => note.id == id)[0];
+//     target.content = content;
+//     saveNote(notes);
+// }
+
+
+// function addNote(){
+//     const notes = getNotes();
+//     const noteObj = {
+//         id: Math.floor(Math.random() * 100000),
+//         content: "",
+//     };
+//     const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+//     appEl.insertBefore(noteEl, btnEl);
+
+//     notes.push(noteObj);
+
+//     saveNote(notes);
+
+// }
+
+// function saveNote(notes){
+//     localStorage.setItem("note-app", JSON.stringify(notes));
+// }
+
+// function getNotes(){
+//    return JSON.parse(localStorage.getItem("note-app") || "[]");
+// }
+
+// btnEl.addEventListener("click",addNote);
+
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
+
+getNotes().forEach((note) => {
+    const noteEl = createNoteEl(note.id,note.content);
+    appEl.insertBefore(noteEl,btnEl);
+});
+
+function createNoteEl(id,content){
+    //console.log(id,content)
+    const element = document.createElement("textarea");
+    element.classList.add("note");
+    element.placeholder = "Empty Note";
+    element.value = content;
+    
+    //console.log(element);
+
+    element.addEventListener("dblclick", () => {
+        const warning = confirm("Do you want to delete this note?");
+        if (warning){
+            deleteNote(id,element);
+        }
+    });
+
+    element.addEventListener("input", () => {
+        updateNote(id, element.value);
+    });
+
+
+    return element;
+
+}
+
+function deleteNote(id,element){
+    const notes = getNotes().filter((note)=>note.id != id);
+    saveNote(notes);
+    appEl.removeChild(element);
+}
+
+function updateNote(id, content){
+    const notes = getNotes();
+    const target = notes.filter((note) => note.id == id)[0];
+    target.content = content;
+    saveNote(notes);
+}
+
+
+function addNote(){
+    const notes = getNotes();
+    const noteObj = {
+        id: Math.floor(Math.random() * 100000),
+        content: "",
+    };
+    const noteEl = createNoteEl(noteObj.id,noteObj.content);
+   
+    appEl.insertBefore(noteEl, btnEl);
+
+    notes.push(noteObj);
+
+    saveNote(notes);
+
+}
+
+function saveNote(notes){
+    localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes(){
+   return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click",addNote);
